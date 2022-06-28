e70ec6c14fb4c1d8f5214877dd4d1233c6bdf0f2
Chandan Gowda <chandangowda574@gmail.com>
2022-06-28 17:32:07 +0530

    Committing new files

---
+++ src/components/search-box/searchbox.components.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './searchbox.styles.css';
+
+export const Searchbox = ({placeholder, handleChange}) => (
+    <input type='search' 
+    className="search"
+    placeholder= {placeholder}
+    onChange={handleChange} />
+)
